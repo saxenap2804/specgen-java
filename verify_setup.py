46a57b1c32fcfd4c536df4b6d0bd19804f6215ce
saxenap2804 <priyankasaxena0109@gmail.com>
2025-06-03 13:31:11 -0500

    Add main SpecGen pipeline script

---
+++ verify_setup.py
@@ -0,0 +1,5 @@
+import torch, transformers, datasets, evaluate 
+print("PyTorch:", torch.__version__) 
+print("Transformers:", transformers.__version__) 
+print("Datasets:", datasets.__version__) 
+print("GPU Available:", torch.cuda.is_available()) 
